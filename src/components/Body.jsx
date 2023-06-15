46c92f6d2ba136fb60d1b9b5383c23a8b0049e8b
tgthiag <tgthiag@gmail.com>
2023-06-15 12:42:12 -0300

    Base layout ok

---
+++ src/components/Body.jsx
@@ -0,0 +1,11 @@
+
+
+function Body(params) {
+    return(
+        <div style={{backgroundColor:'rgba(218,222,218,1)', borderRadius:30, width: "80%", height:"80vh"}}>
+            <p>hello</p>
+        </div>
+    )
+}
+
+export default Body;
