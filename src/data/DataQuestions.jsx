23c0f65de9fb83ead3bd43a66f92f58050a7a82b
tgthiag <tgthiag@gmail.com>
2023-06-19 10:16:44 -0300

    dataClass

---
+++ src/data/DataQuestions.jsx
@@ -0,0 +1,16 @@
+class DataQuestions {
+    constructor(value1, value2, value3, value4, value5, stringValue, date, plant, language, business) {
+      this.value1 = value1;
+      this.value2 = value2;
+      this.value3 = value3;
+      this.value4 = value4;
+      this.value5 = value5;
+      this.comment = stringValue;
+      this.date = date;
+      this.plant = plant;
+      this.lang = language;
+      this.business = business
+    }
+  }
+  
+  export default DataQuestions;
