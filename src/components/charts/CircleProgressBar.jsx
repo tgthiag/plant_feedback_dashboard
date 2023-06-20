e6505efc91d7b408f72e31bfa6d2fca997e20e7f
tgthiag <tgthiag@gmail.com>
2023-06-20 07:54:19 -0300

    New primary views

--- src/components/charts/CircleProgressBar.jsx
+++ src/components/charts/CircleProgressBar.jsx
@@ -1,26 +1,27 @@
-import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 function CircleProgressBar({ data }) {
-    const percentage = 66;
-    console.log(data)
-
 
     const totalAverage = (data.reduce((sum, item) => sum + item.average, 0) / data.length) * 10;
-    console.log(totalAverage.toFixed(1));
 
     return (
         <div style={{ width: 300, height: 300, margin: 10 }}>
             <p style={{ color: "black", fontSize: 18, fontWeight: "bold" }}>Média Geral Mensal</p>
-            <CircularProgressbar value={totalAverage.toFixed(1)} text={`${totalAverage.toFixed(1)}%`} styles={buildStyles({
+            <CircularProgressbarWithChildren value={totalAverage.toFixed(1)}styles={buildStyles({
                 // Text size
                 textSize: '20px',
                 // How long animation takes to go from one percentage to another, in seconds
                 pathTransitionDuration: 2.5,
                 // Colors
-                trailColor: '#d6d6d6',
+
                 backgroundColor: '#3e98c7',
-            })} />
+            })} >
+                <div>
+                    <p style={{color:"#3e98c7", fontWeight:"bold", fontSize: 42}}>{totalAverage.toFixed(1)}%</p>
+                <p style={{color: "gray", fontSize:18}}>Meta: 75%</p>
+                </div>
+            </CircularProgressbarWithChildren>
         </div>
     );
 }
---
+++ src/components/charts/RatesQtyChart.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, LabelList } from 'recharts';
+
+function RatesQtyChart({ itemCountByDate }) {
+    const data = Object.entries(itemCountByDate).map(([date, count]) => ({ date, count }));
+
+    return (
+        <div>
+            <p style={{ color: "black", fontSize: 18, fontWeight: "bold" }}>Quantidade de feedbacks recebidos</p>
+            <BarChart width={1000} height={400} data={data}>
+                <XAxis dataKey="date" />
+                <Tooltip />
+                <Bar dataKey="count" fill="#8884d8" >
+                    <LabelList dataKey="count" position="top" fontSize={16} />
+                </Bar>
+            </BarChart>
+        </div>
+    );
+}
+export default RatesQtyChart;
