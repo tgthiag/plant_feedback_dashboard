c6933e87202b436284085ea91d39fc2bf5ff846c
tgthiag <tgthiag@gmail.com>
2023-06-21 14:44:57 -0300

    CAPITALIZE WORDS AND REMOVE ACCENT MARKS & signs

--- src/functions/capitalizeWords.jsx
+++ src/functions/capitalizeWords.jsx
@@ -1,6 +1,23 @@
-export const capitalizeWords = str => {
-    console.log(str)
-    return str?.charAt(0).toUpperCase() + str?.slice(1).toLowerCase()
+//CAPITALIZE WORDS AND REMOVE ACCENT MARKS
+export const capitalizeWords = (str) => {
+    var word = "";
+    var strAccents = str.split('');
+    var strAccentsOut = new Array();
+    var strAccentsLen = strAccents.length;
+    var accents = 'ÀÁÂÃÄÅàáâãäåÒÓÔÕÕÖØòóôõöøÈÉÊËèéêëðÇçÐÌÍÎÏìíîïÙÚÛÜùúûüÑñŠšŸÿýŽž';
+    var accentsOut = ['A','A','A','A','A','A','a','a','a','a','a','a','O','O','O','O','O','O','O','o','o','o','o','o','o','E','E','E','E','e','e','e','e','e','C','c','D','I','I','I','I','i','i','i','i','U','U','U','U','u','u','u','u','N','n','S','s','Y','y','y','Z','z'];
     
-
+    for (var y = 0; y < strAccentsLen; y++) {
+      if (accents.indexOf(strAccents[y]) !== -1) {
+        strAccentsOut[y] = accentsOut[accents.indexOf(strAccents[y])];
+      } else {
+        strAccentsOut[y] = strAccents[y];
+      }
+    }
+    
+    strAccentsOut = strAccentsOut.join('');
+    word = strAccentsOut.replace("!","").replace(".","").replace(",","").replace("?","");
+    
+    return word?.charAt(0).toUpperCase() + word?.slice(1).toLowerCase();
   };
+  
